5e8653e1d43d408e06a6787ad13190e84afa305e
sayantanmandal009 <157054653+sayantanmandal009@users.noreply.github.com>
2024-06-20 01:00:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ DB_NAME = environ.get("DB_NAME", "Cluster0")
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_USERNAME = environ.get("BOT_USERNAME", "DataGuardian02_bot") # your bot username without @
-PICS = (environ.get('PICS', None)).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
